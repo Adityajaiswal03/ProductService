c2704ae51987a9cdb55d65c722b3efd8fb67cd35
Adityajaiswal03 <git@github.com:Adityajaiswal03/ProductService.git>
2024-04-10 01:30:37 +0530

    added DataBase

--- src/main/java/dev/aditya/productservice/models/category.java
+++ src/main/java/dev/aditya/productservice/models/category.java
@@ -1,11 +1,20 @@
 package dev.aditya.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class category {
+    @Id
     private Long id;
     private String Name;
+    private String Descprition;
 }
--- src/main/java/dev/aditya/productservice/models/product.java
+++ src/main/java/dev/aditya/productservice/models/product.java
@@ -1,15 +1,25 @@
 package dev.aditya.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class product {
+    @Id
     private Long id;
     private String title;
     private String description;
     private double price;
+    @ManyToOne
     private category category;
     private String imageUrl;
 }
